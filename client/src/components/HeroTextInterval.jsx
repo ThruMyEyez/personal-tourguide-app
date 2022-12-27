3460b6b3e19b458b44baff19bbc5ca08afc3e25e
ThruMyEyez <xfeindflug@googlemail.com>
2022-12-27 05:01:40 +0100

    [add] react HeroText component styled with tailwind

---
+++ client/src/components/HeroTextInterval.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useInterval } from "react-use";
+
+const HeroTextInterval = ({ title, strings }) => {
+  const [currentString, setCurrentString] = useState(0);
+
+  useInterval(() => {
+    setCurrentString(strings.length - 1 === currentString ? 0 : currentString + 1);
+  }, 1000);
+
+  return (
+    <div className="flex flex-col items-center font-extrabold whitespace-pre sm:text-2xl">
+      <p className="text-md">{title}</p>
+      {strings.map((string, idx) => {
+        return (
+          <span key={string} className="relative block text-center">
+            <span
+              className={`text-6xl md:text-7xl lg:text-8xl absolute transition duration-1000 ${
+                currentString !== idx ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              {string}
+            </span>
+
+            <span
+              className={`text-6xl md:text-7xl lg:text-8xl bg-clip-text text-transparent bg-gradient-to-r ${
+                (idx === 0 && "from-yellow-400 via-red-500 to-pink-500") ||
+                (idx === 1 && idx === 1 && "from-purple-400 via-pink-500 to-red-500") ||
+                (idx === 2 && "from-green-400 to-blue-500")
+              }`}
+            >
+              {string}
+            </span>
+          </span>
+        );
+      })}
+    </div>
+  );
+};
+
+export default HeroTextInterval;
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,8 +1,14 @@
+import HeroTextInterval from "../components/HeroTextInterval";
+
 const Main = () => {
   return (
-    <div style={{ backgroundColor: "cadetblue" }} className="text-gray-500">
-      <h2>main page</h2>
-      ______
+    <div className="px-4 mt-12 max-w-2xl mx-auto my-auto flex flex-col justify-center gap-12 divide-y divide-gray-200 rounded-lg">
+      <div className="flex flex-col items-center justify-center inset-0">
+        <HeroTextInterval
+          title={"Explore the city in the App & get on"}
+          strings={["Unique Tours", "Rare Events", "Personal Guides"]}
+        />
+      </div>
     </div>
   );
 };
