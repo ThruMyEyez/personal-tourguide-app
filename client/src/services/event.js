f4d12006354840376f564985990712047d51d68e
ThruMyEyez <xfeindflug@googlemail.com>
2023-01-10 12:56:07 +0100

    [update] set comments

--- client/src/services/event.js
+++ client/src/services/event.js
@@ -4,9 +4,12 @@ import { api, authHeaders } from "./api";
 // I think this is easier to understand,
 // because this are the main Public functionality routes
 
+// Get all events/products from the API
 export const getAllEvents = () => api.get("/event/");
 
 export const getSingleEvent = (id) => api.get(`/event/${id}`);
 
+// Customer can create a new Rating if he visited the event
+// and the event is already over.
 export const newEventRating = (data, productId) =>
   api.post(`/event/rating/${productId}`, data, authHeaders);
