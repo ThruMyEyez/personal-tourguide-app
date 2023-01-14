2e980627fd7cdd74e6491f5e6933c424cbe5659d
ThruMyEyez <xfeindflug@googlemail.com>
2023-01-14 17:15:58 +0100

    [fix] Editor & EditorView component

--- client/src/components/Editor/EditorView.jsx
+++ client/src/components/Editor/EditorView.jsx
@@ -1,9 +1,34 @@
-import { EditorContent } from "@tiptap/react";
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Underline from "@tiptap/extension-underline";
+import TextAlign from "@tiptap/extension-text-align";
 
-const EditorView = ({ content }) => {
+const EditorView = ({ content, editable }) => {
+  const viewContent = JSON.parse(content);
+
+  const editor = useEditor({
+    content: viewContent,
+    extensions: [
+      StarterKit,
+      Underline,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+    ],
+    editable: false,
+    editorProps: {
+      // Editor Content ViewBox Tailwind Styling
+      attributes: {
+        class:
+          "prose bg-white min-h-10 prose-sm sm:prose lg:prose-lg xl:prose-2xl m-5 focus:outline-none",
+      },
+    },
+  });
+  //editor.setEditable(false);
+
   return (
     <div>
-      <EditorContent editor={content} />
+      <EditorContent editor={editor} />
     </div>
   );
 };
