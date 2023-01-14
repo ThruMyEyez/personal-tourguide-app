6302f805bca7d52e407c4518211313cc47ca4c30
AHardiment <99344068+AHardiment@users.noreply.github.com>
2023-01-14 17:25:58 +0100

    modified several components
    Added cards to the allproducts component, modified styling on home page and implemented profile picture drop down for dashboard and logout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link, Outlet, useLocation } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../context/authentication";
+import { MdBackpack } from "react-icons/md";
 
 const Navbar = () => {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
@@ -9,21 +10,40 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="flex justify-around items-center w-full p-3 bg-clip-pink-violet">
-        <ul className="flex m-0 list-none gap-x-3">
-          <li className="font-bold pr-3">
+      <nav className="flex lg:p-3 p-5 lg:justify-around text-lg justify-between items-center w-full bg-clip-pink-violet">
+        <ul className="flex m-0 list-none">
+          <li className="font-bold">
             <Link to="/">GuideGo</Link>
           </li>
+          <MdBackpack className="mr-6 my-auto h-100 text-pink-500 inline-flex" />
           <li>
             <Link to="/public-tours">Explore Tours</Link>
           </li>
         </ul>
-        <div className="flex gap-x-1 items-center">
+        <div className="flex items-center">
           {isLoggedIn && (
             <>
-              <span>{user && user.name}</span>
-              <Link to="/dashboard">Dashboard</Link>
-              <button onClick={logOutUser}>Log Out</button>
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                  <div className="w-8 rounded-full">
+                    <img
+                      src="https://placeimg.com/80/80/people"
+                      alt="profile"
+                    />
+                  </div>
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-black"
+                >
+                  <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                  </li>
+                  <li>
+                    <button onClick={logOutUser}>Logout</button>
+                  </li>
+                </ul>
+              </div>
             </>
           )}
           {!isLoggedIn && (
@@ -37,7 +57,7 @@ const Navbar = () => {
                 Sign up
               </Link>
               <Link
-                className="login-btn border-2 border-pink-100 hover:bg-gradient-to-r from:pink-500 to:violet-500 hover:text-white rounded-lg pt-2 pb-2 px-3"
+                className="login-btn border-2 border-pink-100 hover:bg-gradient-to-r from:pink-500 to:violet-500 hover:text-white mr-3 rounded-lg pt-2 pb-2 px-3"
                 to="/login"
                 state={{ background: location }}
               >
